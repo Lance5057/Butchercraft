59eec75b570a2b4060ea40510d6247f67a007fb7
Lance5057 <Lance5057@gmail.com>
2024-06-21 13:06:13 -0500

    2.4!

--- src/main/java/com/lance5057/butchercraft/Butchercraft.java
+++ src/main/java/com/lance5057/butchercraft/Butchercraft.java
@@ -18,7 +18,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class Butchercraft {
 
 	public final static String MOD_ID = "butchercraft";
-	public static final String VERSION = "2.3.17";
+	public static final String VERSION = "2.4.0";
 
 	public static Logger logger = LogManager.getLogger();
 
